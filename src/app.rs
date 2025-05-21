096d8c8ace9b975b2159eadb77961519616d52f0
Emi_ <103583822+jerkey06@users.noreply.github.com>
2025-05-20 18:10:29 -0600

    Feat: Setup basic window and event loop using winit

---
+++ src/app.rs
@@ -0,0 +1,36 @@
+﻿use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Minecraft Clone - Rust!")
+        .build(&event_loop)
+        .unwrap();
+    
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed!");
+                elwt.exit();
+            }
+            Event::AboutToWait => {
+                window.request_redraw();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+            }
+            _ => (),
+        }
+    }).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
+mod app;
+
 fn main() {
-    println!("Hello, future clone!");
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_adds_correctly() {
-        assert_eq!(add(2, 2), 4);
-    }
-
-    #[test]
-    fn another_test_example() {
-        assert_ne!(add(1,1), 3, "1+1 should not be 3");
-    }
+    app::run();
 }
